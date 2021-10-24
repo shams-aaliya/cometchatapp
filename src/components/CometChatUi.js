86193a6eb68a7ef8f2940ef37d78cbb106a770e0
Aaliya <shamsaaliya08@gmail.com>
2021-10-24 21:28:45 +0530

    Login file deleted

--- src/components/CometChatUi.js
+++ src/components/CometChatUi.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {CometChatUI,} from '../cometchat-pro-react-ui-kit/CometChatWorkspace/src';
 // import { CometChat } from '@cometchat-pro/chat';
 
-const CometChatUi = ({userdetail}) => {
+const CometChatUi = () => {
     
     return (
         <div style={{width: '100vw', height:'100vh' }}>
-            <CometChatUI {...userdetail}/>
+            <CometChatUI />
         </div>
     )
 }
